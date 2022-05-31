88a176d84de17913364d3f33637205e38e538fad
Farouk <farouk.houmaidi@equisoft.com>
2022-05-31 15:29:00 -0400

    fixing psalm errors

--- Kronos/Session/RedisSessionHandler.php
+++ Kronos/Session/RedisSessionHandler.php
@@ -22,140 +22,136 @@ namespace Kronos\Session;
  */
 class RedisSessionHandler implements \SessionHandlerInterface
 {
-	/**
-	 * @var \Predis\Client|\Redis
-	 */
-	protected $redis;
+    /**
+     * @var \Redis
+     */
+    protected $redis;
 
-	/**
-	 * @var int
-	 */
-	protected $ttl;
+    /**
+     * @var int
+     */
+    protected int $ttl;
 
-	/**
-	 * @var string
-	 */
-	protected $prefix;
+    /**
+     * @var string
+     */
+    protected string $prefix;
 
-	/**
-	 * @var int Default PHP max execution time in seconds
-	 */
-	const DEFAULT_MAX_EXECUTION_TIME = 30;
+    /**
+     * @var int Default PHP max execution time in seconds
+     */
+    const DEFAULT_MAX_EXECUTION_TIME = 30;
 
-	/**
-	 * @var bool Indicates an sessions should be locked
-	 */
-	protected $locking;
+    /**
+     * @var bool Indicates an sessions should be locked
+     */
+    protected bool $locking;
 
-	/**
-	 * @var bool Indicates an active session lock
-	 */
-	protected $locked;
+    /**
+     * @var bool Indicates an active session lock
+     */
+    protected bool $locked;
 
-	/**
-	 * @var string Session lock key
-	 */
-	private $lockKey;
+    /**
+     * @var string Session lock key
+     */
+    private string $lockKey;
 
-	/**
-	 * @var string Session lock token
-	 */
-	private $token;
+    /**
+     * @var string Session lock token
+     */
+    private string $token;
 
-	/**
-	 * @var int Microseconds to wait between acquire lock tries
-	 */
-	private $spinLockWait;
+    /**
+     * @var int Microseconds to wait between acquire lock tries
+     */
+    private int $spinLockWait;
 
-	/**
-	 * @var int Maximum amount of seconds to wait for the lock
-	 */
-	private $lockMaxWait;
+    /**
+     * @var int Maximum amount of seconds to wait for the lock
+     */
+    private int $lockMaxWait;
 
-	/**
-	 * Redis session storage constructor.
-	 *
-	 * @param \Predis\Client|\Redis $redis   Redis database connection
-	 * @param array                 $options Session options
-	 * @param string                $prefix  Prefix to use when writing session data
-	 */
-	public function __construct($redis, array $options = array(), $prefix = 'session', $locking = true, $spinLockWait = 150000)
-	{
-		$this->redis = $redis;
-		$this->ttl = isset($options['gc_maxlifetime']) ? (int) $options['gc_maxlifetime'] : 0;
-		if (isset($options['cookie_lifetime']) && $options['cookie_lifetime'] > $this->ttl) {
-			$this->ttl = (int) $options['cookie_lifetime'];
-		}
-		$this->prefix = $prefix;
+    /**
+     * Redis session storage constructor.
+     *
+     * @param \Redis $redis Redis database connection
+     * @param array $options Session options
+     * @param string $prefix Prefix to use when writing session data
+     */
+    public function __construct(
+        \Redis $redis,
+        array $options = array(),
+        string $prefix = 'session',
+        bool $locking = true,
+        int $spinLockWait = 150000
+    ) {
+        $this->redis = $redis;
+        $this->ttl = isset($options['gc_maxlifetime']) ? (int)$options['gc_maxlifetime'] : 0;
+        if (isset($options['cookie_lifetime']) && $options['cookie_lifetime'] > $this->ttl) {
+            $this->ttl = (int)$options['cookie_lifetime'];
+        }
+        $this->prefix = $prefix;
+        $this->locking = $locking;
+        $this->locked = false;
+        $this->lockKey = "";
+        $this->spinLockWait = $spinLockWait;
+        $this->lockMaxWait = ini_get('max_execution_time')
+            ? (int)ini_get('max_execution_time')
+            : self::DEFAULT_MAX_EXECUTION_TIME;
+    }
 
-		$this->locking = $locking;
-		$this->locked = false;
-		$this->lockKey = null;
-		$this->spinLockWait = $spinLockWait;
-		$this->lockMaxWait = ini_get('max_execution_time');
-		if (!$this->lockMaxWait) {
-			$this->lockMaxWait = self::DEFAULT_MAX_EXECUTION_TIME;
-		}
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function open($path, $name): bool
+    {
+        return true;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function open($savePath, $sessionName)
-	{
-		return true;
-	}
+    /**
+     * Lock the session data.
+     */
+    protected function lockSession($sessionId): bool
+    {
+        $attempts = (1000000 / $this->spinLockWait) * $this->lockMaxWait;
+        $this->token = uniqid();
+        $this->lockKey = $sessionId . '.lock';
+        for ($i = 0; $i < $attempts; ++$i) {
+            // We try to acquire the lock
+            $setFunction = function ($redis, $key, $token, $ttl) {
+                return $redis->set(
+                    $key,
+                    $token,
+                    array('NX', 'PX' => $ttl)
+                );
+            };
 
-	/**
-	 * Lock the session data.
-	 */
-	protected function lockSession($sessionId)
-	{
-		$attempts = (1000000 / $this->spinLockWait) * $this->lockMaxWait;
+            $success = $setFunction(
+                $this->redis,
+                $this->getRedisKey($this->lockKey),
+                $this->token,
+                $this->lockMaxWait * 1000 + 1
+            );
 
-		$this->token = uniqid();
+            if ($success) {
+                $this->locked = true;
+                return true;
+            }
+            /** @psalm-suppress ArgumentTypeCoercion */
+            usleep($this->spinLockWait);
+        }
 
-		$this->lockKey = $sessionId.'.lock';
-		for ($i = 0;$i < $attempts;++$i) {
+        return false;
+    }
 
-			// We try to aquire the lock
-			$setFunction = function ($redis, $key, $token, $ttl) {
-				if ($redis instanceof \Redis) {
-					return $redis->set(
-						$key,
-						$token,
-						array('NX', 'PX' => $ttl)
-					);
-				} else if($redis instanceof \Predis\Client){
-					return $redis->set(
-						$key,
-						$token,
-						'PX',
-						$ttl,
-						'NX'
-					);
-				}
-			};
-			$success = $setFunction($this->redis, $this->getRedisKey($this->lockKey), $this->token, $this->lockMaxWait * 1000 + 1);
-			if ($success) {
-				$this->locked = true;
-
-				return true;
-			}
-
-			usleep($this->spinLockWait);
-		}
-
-		return false;
-	}
-
-	/**
-	 * Unlock the session data.
-	 */
-	private function unlockSession()
-	{
-		// If we have the right token, then delete the lock
-		$script = <<<LUA
+    /**
+     * Unlock the session data.
+     */
+    private function unlockSession()
+    {
+        // If we have the right token, then delete the lock
+        $script = <<<LUA
 if redis.call("GET", KEYS[1]) == ARGV[1] then
     return redis.call("DEL", KEYS[1])
 else
@@ -163,109 +159,104 @@ else
 end
 LUA;
 
-		if ($this->redis instanceof \Redis) {
-			$this->redis->eval($script, array($this->getRedisKey($this->lockKey), $this->token), 1);
-		} else {
-			$this->redis->eval($script, 1, $this->getRedisKey($this->lockKey), $this->token);
-		}
-		$this->locked = false;
-		$this->token = null;
-	}
+        $this->redis->eval($script, array($this->getRedisKey($this->lockKey), $this->token), 1);
+        $this->locked = false;
+        $this->token = "";
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function close()
-	{
-		if ($this->locking) {
-			if ($this->locked) {
-				$this->unlockSession();
-			}
-		}
+    /**
+     * {@inheritdoc}
+     */
+    public function close(): bool
+    {
+        if ($this->locking) {
+            if ($this->locked) {
+                $this->unlockSession();
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function read($sessionId)
-	{
-		if ($this->locking) {
-			if (!$this->locked) {
-				if (!$this->lockSession($sessionId)) {
-					return false;
-				}
-			}
-		}
+    /**
+     * {@inheritdoc}
+     */
+    public function read($id)
+    {
+        if ($this->locking) {
+            if (!$this->locked) {
+                if (!$this->lockSession($id)) {
+                    return false;
+                }
+            }
+        }
 
-		return $this->redis->get($this->getRedisKey($sessionId)) ?: '';
-	}
+        $result = $this->redis->get($this->getRedisKey($id));
+        if (isset($result)) {
+            return is_array($result) && !empty($result) ? array_values($result)[0] : $result;
+        } else {
+            return '';
+        }
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function write($sessionId, $data)
-	{
-		if (0 < $this->ttl) {
-			$this->redis->setex($this->getRedisKey($sessionId), $this->ttl, $data);
-		} else {
-			$this->redis->set($this->getRedisKey($sessionId), $data);
-		}
+    /**
+     * {@inheritdoc}
+     */
+    public function write($id, $data): bool
+    {
+        if (0 < $this->ttl) {
+            $this->redis->setex($this->getRedisKey($id), $this->ttl, $data);
+        } else {
+            $this->redis->set($this->getRedisKey($id), $data);
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function destroy($sessionId)
-	{
-		$this->redis->del($this->getRedisKey($sessionId));
-		$this->close();
+    /**
+     * {@inheritdoc}
+     */
+    public function destroy($id): bool
+    {
+        $this->redis->del($this->getRedisKey($id));
+        $this->close();
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function gc($lifetime)
-	{
-		return true;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function gc($max_lifetime)
+    {
+        return 1;
+    }
 
-	/**
-	 * Change the default TTL.
-	 *
-	 * @param int $ttl
-	 */
-	public function setTtl($ttl)
-	{
-		$this->ttl = $ttl;
-	}
+    /**
+     * Change the default TTL.
+     */
+    public function setTtl(int $ttl)
+    {
+        $this->ttl = $ttl;
+    }
 
-	/**
-	 * Prepends the given key with a user-defined prefix (if any).
-	 *
-	 * @param string $key key
-	 *
-	 * @return string prefixed key
-	 */
-	protected function getRedisKey($key)
-	{
-		if (empty($this->prefix)) {
-			return $key;
-		}
+    /**
+     * Prepends the given key with a user-defined prefix (if any).
+     */
+    protected function getRedisKey(string $key): string
+    {
+        if (empty($this->prefix)) {
+            return $key;
+        }
 
-		return $this->prefix.$key;
-	}
+        return $this->prefix . $key;
+    }
 
-	/**
-	 * Destructor.
-	 */
-	public function __destruct()
-	{
-		$this->close();
-	}
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        $this->close();
+    }
 }
